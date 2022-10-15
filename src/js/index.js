6c55bc4593ff1aa53e4c7e82927729cf3874fcb7
Lucas Martins Prado Resende <115594145+resendelucasdev@users.noreply.github.com>
2022-10-15 20:46:12 -0300

    Primeiro Projeto - Commit Inicial

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const abas = document.querySelectorAll(".aba");
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function(){
+    
+    if(aba.classList.contains("selecionado")){
+        return;
+    }
+    
+    selecionarAba(aba)
+    mostrarInformacoesDaAba(aba)
+
+    });
+} );
+
+function selecionarAba(aba) {
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
+    aba.classList.add("selecionado");
+    
+}
+
+function mostrarInformacoesDaAba(aba){
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+    informacaoSelecionada.classList.remove("selecionado");
+
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`
+
+    const infomacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba)
+    infomacaoASerMostrada.classList.add("selecionado")
+}
